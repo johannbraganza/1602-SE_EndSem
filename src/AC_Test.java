8f7923788861207821545574d7a7f03cb02a5c25
Student <Student@DESKTOP-ELAADUU>
2018-04-24 13:01:01 +0530

    ver 3.0

--- src/AC_Test.java
+++ src/AC_Test.java
@@ -4,10 +4,19 @@ import org.junit.Test;
 
 public class AC_Test {
 
+	int on = 0;
 	@Test
 	public void test() 
 	{
+		if(on == 0)
+		{
+			System.out.println("Ac On");
+		}
 		
+		else
+		{
+			System.out.println("Ac Off");
+		}
 	}
 
 }
